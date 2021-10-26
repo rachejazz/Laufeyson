692752885f8d6998616b6d84d2540c9da8e17c63
rachel holmes <dishuhiya@gmail.com>
2021-10-27 02:22:39 +0530

    sub done
    Signed-off-by: rachel holmes <dishuhiya@gmail.com>

--- core/utils/subdirectory.py
+++ core/utils/subdirectory.py
@@ -1,21 +1,59 @@
 import re
 from . import whichcms as wh
 
-def sitemap_crawl(self, q):
-	pass
+def sitemap_crawl(self, q, data = ""):
+	sm_reg_rob = re.findall(r'\n*Sitemap: (.*)', data)
+	sm_reg_raw = re.findall(r'/sitemaps?\.\w+', data)
+	if sm_reg_rob:
+		self.info(f"Sitemap found: {sm_reg_rob[0]}")
+		return True
+	elif sm_reg_raw:
+		self.info(f"Sitemap found: https://{q}{sm_reg_raw[0]}")
+		return True
+	else:
+		return False
+
+def raw(self, q):
+	try:
+		res = self.request('GET', q)
+		data = res.text
+	except:
+		self.error(f"subdirectory: Something went wrong. Please try again.")
+	else:
+		sitemap_crawl(self, q, data)
+		dir_reg = re.findall(r'href=\"(/[\w\-.]+)', data)
+		if dir_reg:
+			dir_reg = list(set(dir_reg))
+			for each in dir_reg:
+				print(f"Checking for {each}")
+				if "." in each:
+					print(each)
+					dir_reg.remove(each)
+				else:
+					print(f"Not in {each}")
+			return dir_reg
+		else:
+			return []
 
 def robots(self, q):
 	self.info(f"subdirectory: Searching for robots.txt...")
 	url = f"{q}/robots.txt"
-	res = self.request('HEAD', url)
+	res = self.request('GET', url)
 	if res.status_code == 404:
-		print(res.url)
 		self.error("No robots.txt found.")
 	else:
-		res = self.request('GET', url)
 		data = res.text
-		rob_dirs = re.findall(r'(?<!(:/))(/[\w\.-]+)/*', data)
-		print(rob_dirs)
+#		dirs = re.findall(r'(?<!(:/))(/[\w\.-]+)/*', data)
+		dirs = re.findall(r'Disallow: (/[\w\-_]*)', data)
+		sitemap_crawl(self, q, data)
+		if dirs:
+			return list(set(dirs))
+		else:
+			return []
+
+def cms(self, q):
+	tech_list = list(filter(lambda x: x not in ['A', 'The'], wh.run(self, q)))
+	return tech_list
 
 def cert(self, q):
 	pass
@@ -24,17 +62,15 @@ def js(self, q):
 	pass
 
 def run(self, q):
+	subdirs = []
 	try:
 		self.info(f"subdirectory: Scanning {q}...")
-#		url = f""
-#		res = self.request('GET',url)
-#		data = res.text
-#		data_json = res.json()
 	except:
 		self.error(f"subdirectory: Something went wrong. Please try again.")
 	else:
-		tech_list = list(filter(lambda x: x not in ['A', 'The'], wh.run(self, q)))
-		print(tech_list)
-		robots(self, q)
-#	if tech_list:
-#		return tech_list
+#		utils = ['cms', 'robots', 'raw', 'cert', 'js']
+		utils = ['raw']
+#		subdirs.extend(list(set(robots(self, q))))
+#		print(subdirs)
+		for each in utils:
+			self.list_output(globals()[each](self, q))
