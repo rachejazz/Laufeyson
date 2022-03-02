4debe99e69239d9ac4ccdfbb69e3803e044c3317
rachel holmes <dishuhiya@gmail.com>
2022-03-03 03:03:57 +0530

    done
    Signed-off-by: rachel holmes <dishuhiya@gmail.com>

--- core/utils/norton.py
+++ core/utils/norton.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import re
 
 def run(self, q):
-	q = q.split('_')[0]
 	send_record = ''
 	url = f'https://safeweb.norton.com/report/show?url={q}'
 	self.info('norton: Scanning domain...')
